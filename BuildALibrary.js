c7b517e53c32660c425332778d73512d7b8ddf30
Kris McNair <kmcnair_100@outlook.com>
2024-04-10 16:19:19 -0400

    add additionsl classes

--- BuildALibrary.js
+++ BuildALibrary.js
@@ -86,3 +86,64 @@ class CD extends Media {
     }
 }
 
+class School {
+    constructor(name, level, numberOfStudents) {
+      this._name = name;
+      this._level = level;
+      this._numberOfStudents = numberOfStudents;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    get level() {
+      return this._level;
+    }
+  
+    get numberOfStudents() {
+      return this._numberOfStudents;
+    }
+  
+    set numberOfStudents(newNumberOfStudents) {
+      if (typeof newNumberOfStudents === 'number') {
+        this._numberOfStudents = newNumberOfStudents;
+      } else {
+        console.log('Invalid input: numberOfStudents must be set to a Number.');
+        }
+      }    
+  
+    quickFacts () {
+      console.log(`${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level.`);
+    }
+    
+    static pickSubstituteTeacher(substituteTeachers) {
+      const randInt = Math.floor(substituteTeachers.length * Math.random());
+      return substituteTeachers[randInt];
+    }
+  }
+  
+  
+  class PrimarySchool extends School {
+    constructor(name, numberOfStudents, pickupPolicy) {
+      super(name, 'primary', numberOfStudents);
+      this._pickupPolicy = pickupPolicy;
+    }    
+    
+    get pickupPolicy() {
+      this._pickupPolicy;
+    }
+  }
+  
+  
+  class HighSchool extends School {
+    constructor(name, numberOfStudents, sportsTeams) {
+      super(name, 'high', numberOfStudents);
+      this._sportsTeams = sportsTeams;
+    }
+  
+    get sportsTeams() {
+      return this._sportsTeams;
+    }
+  }
+  
